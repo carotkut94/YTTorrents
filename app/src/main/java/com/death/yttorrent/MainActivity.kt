44c7bb50cd0a4a164887b327bd37ff17db87e190
DeathCode <carotkut12@gmail.com>
2018-04-18 14:58:15 +0530

    Added storage permissions

--- app/src/main/java/com/death/yttorrent/MainActivity.kt
+++ app/src/main/java/com/death/yttorrent/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.death.yttorrent
 
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -26,7 +27,12 @@ import com.github.se_bastiaan.torrentstream.TorrentOptions
 import com.github.se_bastiaan.torrentstream.listeners.TorrentListener
 import java.lang.Exception
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
+import android.support.v4.app.ActivityCompat
+import android.support.v4.app.ActivityCompat.requestPermissions
+import java.util.jar.Manifest
 
 
 class MainActivity : AppCompatActivity() {
@@ -37,7 +43,7 @@ class MainActivity : AppCompatActivity() {
 
     private var disposable: Disposable? = null
     private var linearLayout: GridLayoutManager? = null
-    private var toolBar:Toolbar? = null
+    private var toolBar: Toolbar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -68,47 +74,50 @@ class MainActivity : AppCompatActivity() {
                                     recyclerView,
                                     object : MoviesAdapter.ClickListener {
                                         override fun onClick(view: View, position: Int) {
-                                            Toast.makeText(view.context, result.data.movies!![position]!!.title, LENGTH_LONG).show()
-                                            val torrentOptions = TorrentOptions.Builder()
-                                                    .saveLocation(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS))
-                                                    .removeFilesAfterStop(true)
-                                                    .build()
+                                            if(isStoragePermissionGranted()) {
+                                                Toast.makeText(view.context, result.data.movies!![position]!!.title, LENGTH_LONG).show()
+                                                val torrentOptions = TorrentOptions.Builder()
+                                                        .saveLocation(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS))
+                                                        .removeFilesAfterStop(true)
+                                                        .build()
 
-                                            val torrentStream = TorrentStream.init(torrentOptions)
-                                            torrentStream.startStream(result.data.movies[position]!!.torrents!![0]!!.url)
+                                                val torrentStream = TorrentStream.init(torrentOptions)
+                                                torrentStream.startStream(result.data.movies[position]!!.torrents!![0]!!.url)
 
-                                            torrentStream.addListener(object : TorrentListener{
-                                                override fun onStreamReady(p0: Torrent?) {
-                                                    Log.e("Ready", "True")
-                                                    Log.d("READY", "onStreamReady: " + p0!!.videoFile)
-                                                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(p0!!.videoFile.toString()))
-                                                    intent.setDataAndType(Uri.parse(p0!!.videoFile.toString()), "video/mp4")
-                                                    startActivity(intent)
-                                                }
+                                                torrentStream.addListener(object : TorrentListener {
+                                                    override fun onStreamReady(p0: Torrent?) {
+                                                        Log.e("Ready", "True")
+                                                        Log.d("READY", "onStreamReady: " + p0!!.videoFile)
+                                                        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(p0!!.videoFile.toString()))
+                                                        intent.setDataAndType(Uri.parse(p0!!.videoFile.toString()), "video/mp4")
+                                                        startActivity(intent)
+                                                    }
 
-                                                override fun onStreamPrepared(p0: Torrent?) {
-                                                    Log.e("Prepared", "True")
-                                                }
+                                                    override fun onStreamPrepared(p0: Torrent?) {
+                                                        Log.e("Prepared", "True")
+                                                    }
 
-                                                override fun onStreamStopped() {
-                                                    Log.e("Stopped", "True")
-                                                }
+                                                    override fun onStreamStopped() {
+                                                        Log.e("Stopped", "True")
+                                                    }
 
-                                                override fun onStreamStarted(p0: Torrent?) {
-                                                    Log.e("Started", "True")
-                                                }
+                                                    override fun onStreamStarted(p0: Torrent?) {
+                                                        Log.e("Started", "True")
+                                                    }
 
-                                                override fun onStreamProgress(p0: Torrent?, p1: StreamStatus?) {
-                                                    Log.e("Progress", ""+p1!!.progress)
-                                                }
+                                                    override fun onStreamProgress(p0: Torrent?, p1: StreamStatus?) {
+                                                        Log.e("Progress", "" + p1!!.progress)
+                                                    }
 
-                                                override fun onStreamError(p0: Torrent?, p1: Exception?) {
-                                                    Log.e("Error", "True")
-                                                    p1!!.printStackTrace()
-                                                }
+                                                    override fun onStreamError(p0: Torrent?, p1: Exception?) {
+                                                        Log.e("Error", "True")
+                                                        p1!!.printStackTrace()
+                                                    }
 
-                                            })
+                                                })
+                                            }
                                         }
+
                                         override fun onLongClick(view: View?, position: Int) {
 
                                         }
@@ -127,5 +136,36 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         disposable?.dispose()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                             permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            1 -> {
+
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+
+                    Log.i("Denied", "Permission has been denied by user")
+                } else {
+                    Log.i("Granted", "Permission has been granted by user")
+                }
+            }
+        }
+    }
+
+    fun isStoragePermissionGranted(): Boolean {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v("Status", "Permission is granted")
+                return true
+            } else {
+                requestPermissions(this, arrayOf(WRITE_EXTERNAL_STORAGE), 1)
+                return false;
+            }
+        } else {
+            Log.v("GRANTED", "Permission is granted")
+            return true
+        }
     }
 }
